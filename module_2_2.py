6921fdf2afdd453ad66f32dcb71a2c4f0c7df87b
Олег <oleg3007kov@gmail.com>
2024-10-07 21:18:31 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -5,5 +5,8 @@ if numbe1 == numbe2 and numbe1 == numbe3:
     print (3)
 elif numbe1 == numbe2 or numbe1 == numbe3 or numbe2 == numbe3:
     print (2)
-elif numbe1 != numbe2 and numbe1 != numbe3:
+else:
     print (0)
+
+# elif numbe1 != numbe2 and numbe1 != numbe3:
+   # print (0)
