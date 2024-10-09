44e46f8a4aa8a170cc69d5acf0a04e957ff08857
Олег <oleg3007kov@gmail.com>
2024-10-10 00:55:57 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,8 @@
 my_string = input() #"Как меняется язык, на котором мы говорим? "
-print (my_string . upper())
-print (my_string . lower())
-print (my_string . replace (" ",""))
-print (my_string . replace (" ","#"))
+print (my_string . upper()) # в ВЕРХНЕМ регистре
+print (my_string . lower())# в нижнеем регистре
+print (my_string . replace (" ","")) # удаление пробелов
+print (my_string . replace (" ","#")) # замена пробелов символом #
 print (my_string . replace ("язык, на котором мы говорим","запах, который мы нюхаем"))
+print (my_string [0]) # 1- символ строки
+print (my_string [-1]) # оследний символ строки
